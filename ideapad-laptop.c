06b73deb6b05d9de42a966b0cd98a233e0f35570
jeremyb31 <jeremy53561@gmail.com>
2018-12-18 16:37:31 -0600

    add Lenovo ideapad S130-11IGM to rfkill quirks

--- ideapad-laptop.c
+++ ideapad-laptop.c
@@ -992,6 +992,13 @@ static const struct dmi_system_id no_hw_rfkill_list[] = {
 		},
 	},
 	{
+		.ident = "Lenovo ideapad S130-11IGM",
+		.matches = {
+		        DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+		        DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo ideapad S130-11IGM"),
+		},
+	},
+	{
 		.ident = "Lenovo ideapad Y700-15ACZ",
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
